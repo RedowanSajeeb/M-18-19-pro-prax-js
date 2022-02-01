13ccc77ed5128746a792234c221974ac98644950
redowansajeeb <redowansajeeb72@gmail.com>
2022-02-02 04:56:08 +0600

    js-commit-done

---
+++ condi.js
@@ -0,0 +1,86 @@
+
+var conDitionPrice = 600;
+var secindPrice = 600;
+if (conDitionPrice > secindPrice){
+    console.log( 'ami kenbo e kenbo');
+}
+else if (conDitionPrice >= secindPrice){
+    console.log('den vi');
+}
+else {
+    console.log('kenbo nah! ami gorib pera nie');
+}
+
+// -----------------
+var webdev = "in Sha Allah";
+if (webdev == "in Sha Allah"){
+    console.log( "huhu re mari ALhamdulillah");
+}
+// --------------
+var dimPrice = 40;
+var aloPrice = 50;
+var rutiPrice = 100;
+var samucha = 60;
+var otiriktoPrice =80;
+var mYprice = 300;
+var moto = (dimPrice + aloPrice + rutiPrice + samucha);
+console.log(moto);
+ 
+if(mYprice >= moto && otiriktoPrice <= mYprice ){
+    console.log( "Ami Happy HuHu!!")
+}
+else{
+    console.log("lagto Nah")
+}
+// --------------------------
+
+var freelancer = true;
+var Home = true;
+var tk = 5000;
+var webdevloper = true;
+var job = false;
+var gari = 2;
+
+if ( (freelancer == true && tk > 1000) && ( Home == true && job == false) && ( gari < 3 && webdevloper == true )  ) {
+    console.log(
+       " yuho bro-congra" );
+    }
+
+else if ( tk > 2000){
+    console.log("congrts");
+}
+
+else {
+    console.log(
+        "ops-bro"
+    )
+}
+
+// ;;;;;;;;;;;;;;;;-----------
+var coco_coklet = 50;
+var alops = 40;
+var potato = 10;
+var icerem = 5;
+var mYbgt = 10;
+var atlest = alops + potato ;
+console.log(atlest);
+
+if ( mYbgt > atlest){
+console.log("vi den upnar alops");
+
+}
+else if ( mYbgt >= coco_coklet ){
+    console.log("vi aita E den");
+    // if( mYbgt >82){
+    //     console.log("dur-vi");
+    // }
+
+}
+else if( mYbgt > icerem){
+    console.log(
+        " yes-vi-done"
+    )
+}
+else {
+    console.log("ar nah")
+}
